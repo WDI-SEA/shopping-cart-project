d9617531249430c44971bdcdad470606dc061ac1
ttmuangkeo <tim.muangkeo@gmail.com>
2017-04-07 16:24:13 -0700

    created wireframe

--- solution-code/main.js
+++ solution-code/main.js
@@ -1,1 +1,25 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var groceryToBeMove = document.getElementById("groceryitem");
+    var shoppingToBeMove = document.getElementById("shoppinglist");
 
+    var onClickGrocery;
+    var onClickShopping = function() {
+        groceryToBeMove.appendChild(this);
+        this.onClick = groceryToBeMove;
+        return false;
+    };
+    onClickGrocery = function() {
+        shoppingToBeMove.appendChild(this);
+        this.onClick = onClickShopping;
+        return false;
+    };
+    for (var i = 0; i < groceryToBeMove.getElementsByTagName("li").length; i++)
+        groceryToBeMove.getElementsByTagName("li")[i].onclick = onClickGrocery;
+
+
+    for (var i = 0; i < shoppingToBeMove.getElementsByTagName("li").length; i++)
+        shoppingToBeMove.getElementsByTagName("li")[i].onclick = onClickShopping;
+
+
+
+})
