f0f7594ce6c34a10596c4d988cbe799cc8518879
jblneely <bowman.neely@gmail.com>
2017-04-07 15:46:17 -0700

    html start

--- solution-code/main.js
+++ solution-code/main.js
@@ -1,1 +1,22 @@
+document.addEventListener("DOMContenetLoaded", function() {
 
+    var cereal = {
+
+    }
+
+    var juices = {
+
+    }
+
+    var candy = {
+
+    }
+
+    var btn = document.getElementById('button')
+    btn.addEventListener(click ', function(event) {
+        console.log(event)
+    })
+
+});
+
+//use document.createElement to add items to UL shoppinglist.
