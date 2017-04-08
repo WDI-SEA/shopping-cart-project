23b8c2b2c8971cb8ddf2f65530db2a3cbbaf83c4
jblneely <bowman.neely@gmail.com>
2017-04-07 17:09:16 -0700

    js start

--- solution-code/main.js
+++ solution-code/main.js
@@ -1,9 +1,24 @@
-document.addEventListener("DOMContenetLoaded", function() {
+document.addEventListener("DOMContentLoaded", function() {
 
-    document.createElement to add items to UL shoppinglist.
+    //document.createElement to add items to UL shoppinglist.
 
+    // var cereal = ['cheerios', 'crunch', 'fruit-loops', 'puffs', 'raisin-bran']
+    // var juice = ['apple-juice', 'carrot-juice', 'grape-juice', 'orange-juice', 'tomato-juice']
+    // var candy = ['kit-kat', 'm&ms', 'skittles', 'snickers', 'twix']
+
+    // var products = ['cereal', 'juice', 'candy']
+
+    //var cereal = getItemById
+
+    var lis = document.querySelectorAll("#category-nav li");
+    lis[0].addEventListener("click", function() {
+        console.log("click")
+        console.log(this.innerText)
+    });
+
+    //for ()
 });
-
+//object.addEventListener("click", script)
 
 
 
@@ -13,3 +28,7 @@ document.addEventListener("DOMContenetLoaded", function() {
 // btn.addEventListener(click ', function(event) {
 //     console.log(this)
 // })
+
+
+
+//remove items from shoppingcart ul: http://stackoverflow.com/questions/14418451/click-event-on-dynamically-generated-list-items-using-jquery
