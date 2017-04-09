2894e6917842948a4b41aff00aa96c66e74999d6
ttmuangkeo <tim.muangkeo@gmail.com>
2017-04-09 14:18:17 -0700

    updated code

--- solution-code/main.js
+++ solution-code/main.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var groceryToBeMove = document.getElementById("groceryitem");
+    var groceryToBeMove = document.getElementById("candylist");
     var shoppingToBeMove = document.getElementById("shoppinglist");
 
     var onClickGrocery;
@@ -13,6 +13,29 @@ document.addEventListener("DOMContentLoaded", function() {
         this.onClick = onClickShopping;
         return false;
     };
+
+    for (var i = 0; i < groceryToBeMove.getElementsByTagName("li").length; i++)
+        groceryToBeMove.getElementsByTagName("li")[i].onclick = onClickGrocery;
+
+
+    for (var i = 0; i < shoppingToBeMove.getElementsByTagName("li").length; i++)
+        shoppingToBeMove.getElementsByTagName("li")[i].onclick = onClickShopping;
+
+    var groceryToBeMove = document.getElementById("juicelist");
+    var shoppingToBeMove = document.getElementById("shoppinglist");
+
+    var onClickGrocery;
+    var onClickShopping = function() {
+        groceryToBeMove.appendChild(this);
+        this.onClick = groceryToBeMove;
+        return false;
+    };
+    onClickGrocery = function() {
+        shoppingToBeMove.appendChild(this);
+        this.onClick = onClickShopping;
+        return false;
+    };
+
     for (var i = 0; i < groceryToBeMove.getElementsByTagName("li").length; i++)
         groceryToBeMove.getElementsByTagName("li")[i].onclick = onClickGrocery;
 
@@ -21,5 +44,27 @@ document.addEventListener("DOMContentLoaded", function() {
         shoppingToBeMove.getElementsByTagName("li")[i].onclick = onClickShopping;
 
 
+
+    var groceryToBeMove = document.getElementById("cereallist");
+    var shoppingToBeMove = document.getElementById("shoppinglist");
+
+    var onClickGrocery;
+    var onClickShopping = function() {
+        groceryToBeMove.appendChild(this);
+        this.onClick = groceryToBeMove;
+        return false;
+    };
+    onClickGrocery = function() {
+        shoppingToBeMove.appendChild(this);
+        this.onClick = onClickShopping;
+        return false;
+    };
+
+    for (var i = 0; i < groceryToBeMove.getElementsByTagName("li").length; i++)
+        groceryToBeMove.getElementsByTagName("li")[i].onclick = onClickGrocery;
+
+
+    for (var i = 0; i < shoppingToBeMove.getElementsByTagName("li").length; i++)
+        shoppingToBeMove.getElementsByTagName("li")[i].onclick = onClickShopping;
 
 })
