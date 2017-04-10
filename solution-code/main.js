74afc1716bdd7a3234d9e4821a4c48f3e943b828
stevenkeiser <stevekeiser@gmail.com>
2017-04-10 06:38:49 -0700

    logic updated for categories

--- solution-code/main.js
+++ solution-code/main.js
@@ -1,43 +1,43 @@
 // MVP-click events for categories,display html products and hide other divs using add/remove class,doument load
-// var flora = document.getElementById('flora');
-// var fauna = document.getElementById('fauna');
-// var insecta = document.getElementById('insecta');
+
 var cart = document.getElementById('cart');
-// var catFlora = document.getElementById('category-flora');
-// var catFauna = document.getElementById('category-fauna');
-// var catInsecta = document.getElementById('category-insecta');
+
 var productList = document.querySelectorAll('.products');
 var categoryList = document.querySelectorAll('.category');
 var categoryNav = document.querySelectorAll('.cat-nav');
 
-for (var i = 0; i < productList.length; i++) {
-    productList[i].addEventListener('click', addToCart);
-}
-
-for (var i = 0; i < categoryNav.length; i++) {
-    categoryNav[i].addEventListener('click', changeCat);
-}
-
-
-function addToCart() {
-    // var addItem = item;
-    var newItem = document.createElement('li');
-    newItem.innerText = (this.id);
-    cart.appendChild(newItem);
-    newItem.addEventListener('click', removeFromCart)
-}
-
-function removeFromCart() {
-    this.classList.add('hidden');
-}
-
-
-
-function changeCat() {
-    var referral = this;
-    for (var i = 0; i < categoryList.length; i++) {
-        categoryList[i].classList.add('hidden');
-        console.log(this.id);
-        // now to unhide the active one
+function onLoad() {
+    for (var i = 0; i < productList.length; i++) {
+        productList[i].addEventListener('click', addToCart);
     }
+
+    for (var i = 0; i < categoryNav.length; i++) {
+        categoryNav[i].addEventListener('click', changeCat);
+    }
+
+
+    function addToCart() {
+        // var addItem = item;
+        var newItem = document.createElement('li');
+        newItem.innerText = (this.id);
+        cart.appendChild(newItem);
+        newItem.addEventListener('click', removeFromCart)
+    }
+
+    function removeFromCart() {
+        this.classList.add('hidden');
+    }
+
+
+
+    function changeCat() {
+        var referral = this.getAttribute('data-category');
+        console.log(referral);
+        for (var i = 0; i < categoryList.length; i++) {
+            categoryList[i].classList.add('hidden');
+        }
+        document.getElementById(referral).classList.remove('hidden');
+    }
+
 }
+document.addEventListener("DOMContentLoaded", onLoad);
