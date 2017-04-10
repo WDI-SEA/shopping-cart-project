31b42a0f212258f74cfa5447c94c84a85334a5fa
buffalocheck <shawn@buffalocheck.com>
2017-04-09 21:03:18 -0700

    when I added CSS lines 12-18 it somehow disabbles my JS that hides the categories.

--- solution-code/main.js
+++ solution-code/main.js
@@ -1,1 +1,24 @@
+//done - nav items class = hidden
+//done - listen for click -- remove hidden & also reassign hidden to the other 2 - 3 diff cases
+//??: when I added CSS lines 12-18 it de-activates my listen events in JS below. Weird!
+//to do: listen for click text -- put that text in shopping cart
 
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("cerealButt").addEventListener("click", function() {
+        document.getElementById("cerealId").classList.remove("hidden");
+        document.getElementById("literatureId").classList.add("hidden");
+        document.getElementById("soupId").classList.add("hidden");
+    });
+    //
+    document.getElementById("litButt").addEventListener("click", function() {
+        document.getElementById("literatureId").classList.remove("hidden");
+        document.getElementById("cerealId").classList.add("hidden");
+        document.getElementById("soupId").classList.add("hidden");
+    });
+    //
+    document.getElementById("soupButt").addEventListener("click", function() {
+        document.getElementById("soupId").classList.remove("hidden");
+        document.getElementById("literatureId").classList.add("hidden");
+        document.getElementById("cerealId").classList.add("hidden");
+    });
+});
