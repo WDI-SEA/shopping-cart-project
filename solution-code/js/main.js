512661b851556e62b9fb6efdafb1a87a52e28277
emaquino44 <prexy44@yahoo.com>
2017-04-09 18:54:41 -0700

    added click function-still working on it

--- solution-code/js/main.js
+++ solution-code/js/main.js
@@ -1,1 +1,7 @@
+var myCandy = ["kit-kat".this, "snickers".this, "m&ms".this, "twix".this];
+console.log("kit-kat");
 
+
+function myFunction() {
+    document.getElementbyId("m&ms").index.html = "M&M's";
+}
