a1096348b8de81f492c9e41ee2dcc306ae49b7d7
emaquino44 <prexy44@yahoo.com>
2017-04-10 00:04:57 -0700

    added onclick functions and updated css styles

--- solution-code/js/main.js
+++ solution-code/js/main.js
@@ -1,7 +1,59 @@
-var myCandy = ["kit-kat".this, "snickers".this, "m&ms".this, "twix".this];
-console.log("kit-kat");
-
-
 function myFunction() {
-    document.getElementbyId("m&ms").index.html = "M&M's";
+    var item1 = document.getElementById('item1');
+    item1.innerHTML = 'M&Ms';
+}
+
+function myFunction2() {
+    var item2 = document.getElementById('item2');
+    item2.innerHTML = 'Twix';
+}
+
+function myFunction3() {
+    var item3 = document.getElementById('item3');
+    item3.innerHTML = 'Kit-Kit';
+}
+
+function myFunction4() {
+    var item4 = document.getElementById('item4');
+    item4.innerHTML = 'Snickers';
+}
+
+function myFunction5() {
+    var item5 = document.getElementById('item5');
+    item5.innerHTML = 'Grape Juice';
+}
+
+function myFunction6() {
+    var item6 = document.getElementById('item6');
+    item6.innerHTML = 'Tomato Juice';
+}
+
+function myFunction7() {
+    var item7 = document.getElementById('item7');
+    item7.innerHTML = 'Carrot Juice';
+}
+
+function myFunction8() {
+    var item8 = document.getElementById('item8');
+    item8.innerHTML = 'Orange Juice';
+}
+
+function myFunction9() {
+    var item9 = document.getElementById('item9');
+    item9.innerHTML = 'Cheerios';
+}
+
+function myFunction10() {
+    var item10 = document.getElementById('item10');
+    item10.innerHTML = 'Capitan Crunch';
+}
+
+function myFunction11() {
+    var item11 = document.getElementById('item11');
+    item11.innerHTML = 'Raisin Bran';
+}
+
+function myFunction12() {
+    var item12 = document.getElementById('item12');
+    item12.innerHTML = 'Puffs';
 }
