e995fe5d74785dca8093555be22f6949e7256f74
buffalocheck <shawn@buffalocheck.com>
2017-04-10 19:14:48 -0700

    this was hard. I just burned out on this one.

--- solution-code/main.js
+++ solution-code/main.js
@@ -1,6 +1,3 @@
-//done - nav items class = hidden
-//done - listen for click -- remove hidden & also reassign hidden to the other 2 - 3 diff cases
-//??: when I added CSS lines 12-18 it de-activates my listen events in JS below. Weird!
 //to do: listen for click text -- put that text in shopping cart
 
 document.addEventListener("DOMContentLoaded", function() {
@@ -21,4 +18,16 @@ document.addEventListener("DOMContentLoaded", function() {
         document.getElementById("literatureId").classList.add("hidden");
         document.getElementById("cerealId").classList.add("hidden");
     });
+    $("p").on("click", function() {
+        var listItem = $(this).html();
+        $("#cart").append(listItem);
+    });
+
+
+
+
 });
+//end
+
+// $(function() {
+// });
